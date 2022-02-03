f93fef66621a76cefbf734994603f62625bf890e
stefanom13 <stefanomancuso13@gmail.com>
2022-02-03 17:28:42 +0100

    inserimento prompt password

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
+// prima operazione 
+
 const userName = prompt('Come ti chiami?')
 
-console.log(userName)
+console.log(userName) // per visualizzare in console 
+
+
+const passWord = prompt('Inserisci la tua password')
+
+console.log(passWord)
+
+
+
+// seconda operazione (per selezionare id saluto)
+
+const salutoHTMLElement = document.getElementById('saluto')
+
+console.log(salutoHTMLElement)
+
+
+const passwordHTMLElement = document.getElementById('password')
+
+console.log(passwordHTMLElement)
+
+
+
+
+// terza operazione (visualizzare html+javascript)
+
+salutoHTMLElement.innerHTML += ' ' + userName // += per concatenare e salvare nell innerhtml dell elemento
+
+passwordHTMLElement.innerHTML += ' ' + passWord
+
+// password 
