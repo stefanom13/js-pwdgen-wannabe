1633a7ad8368bbbfac98be4cc87db47575f7f5d4
stefanom13 <stefanomancuso13@gmail.com>
2022-02-03 18:14:03 +0100

    inserimento dati principali

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+const currentYear = 2022
+
 // prima operazione 
-
 const userName = prompt('Come ti chiami?')
-
 console.log(userName) // per visualizzare in console 
 
+const userAge = prompt ('Inserisci anno di nascita')
+console.log(userAge)
 
 const passWord = prompt('Inserisci la tua password')
-
 console.log(passWord)
+
+const colorPreferito = prompt('Inserisci il tuo colore preferito')
+console.log(colorPreferito)
+
+const birth = currentYear - userAge
+console.log(birth)   // calcolo età
+
 
 
 
@@ -17,10 +25,17 @@ const salutoHTMLElement = document.getElementById('saluto')
 
 console.log(salutoHTMLElement)
 
+const ageHTMLElement = document.getElementById('age')
+console.log(ageHTMLElement)
 
 const passwordHTMLElement = document.getElementById('password')
 
 console.log(passwordHTMLElement)
+
+
+const colorPreferitoHTMLElement = document.getElementById('colorPref')
+
+console.log(colorPreferitoHTMLElement)
 
 
 
@@ -31,4 +46,9 @@ salutoHTMLElement.innerHTML += ' ' + userName // += per concatenare e salvare ne
 
 passwordHTMLElement.innerHTML += ' ' + passWord
 
-// password 
+colorPreferitoHTMLElement.innerHTML += ' ' + colorPreferito
+
+ageHTMLElement.innerHTML += ' ' + birth + ' ' + 'anni'
+
+// colorPreferitoHTMLElement.innerHTML += ` ${colorPreferito}`  // modo alternativo per scrivere riga36
+
