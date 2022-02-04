c65676207d5dbbe38983bfd4206a999fe45bb677
stefanom13 <stefanomancuso13@gmail.com>
2022-02-04 16:30:07 +0100

    correzzione esercizio

--- app.js
+++ app.js
@@ -1,54 +1,57 @@
-const currentYear = 2022
+const currentYear = 2022 ;
 
 // prima operazione 
-const userName = prompt('Come ti chiami?')
-console.log(userName) // per visualizzare in console 
+const userName = prompt('Come ti chiami?');
+console.log(userName); // per visualizzare in console 
 
-const userAge = prompt ('Inserisci anno di nascita')
-console.log(userAge)
+const userAge = prompt ('Inserisci anno di nascita');
+console.log(userAge);
 
-const passWord = prompt('Inserisci la tua nuova password')
-console.log(passWord)
 
-const colorPreferito = prompt('Inserisci il tuo colore preferito')
-console.log(colorPreferito)
+const colorPreferito = prompt('Inserisci il tuo colore preferito');
+console.log(colorPreferito);
 
-const birth = currentYear - userAge
-console.log(birth)   // calcolo età
+const birth = currentYear - userAge;
+console.log(birth);   // calcolo età
+
+const password =`${userName}${birth}${colorPreferito}`;
+console.log(password);
+
+// stampare password
+document.getElementById('password').innerHTML += password ;
 
 
 
 
 // seconda operazione (per selezionare id saluto)
 
-const salutoHTMLElement = document.getElementById('saluto')
+// const salutoHTMLElement = document.getElementById('saluto')
 
-console.log(salutoHTMLElement)
+// console.log(salutoHTMLElement)
 
-const ageHTMLElement = document.getElementById('age')
-console.log(ageHTMLElement)
+// const ageHTMLElement = document.getElementById('age')
+// console.log(ageHTMLElement)
 
-const passwordHTMLElement = document.getElementById('password')
+// const passwordHTMLElement = document.getElementById('password')
 
-console.log(passwordHTMLElement)
+// console.log(passwordHTMLElement)
 
 
-const colorPreferitoHTMLElement = document.getElementById('colorPref')
+// const colorPreferitoHTMLElement = document.getElementById('colorPref')
 
-console.log(colorPreferitoHTMLElement)
+// console.log(colorPreferitoHTMLElement)
 
 
 
 
 // terza operazione (visualizzare html+javascript)
 
-salutoHTMLElement.innerHTML += ' ' + userName // += per concatenare e salvare nell innerhtml dell elemento
+// salutoHTMLElement.innerHTML += ' ' + userName // += per concatenare e salvare nell innerhtml dell elemento
 
-passwordHTMLElement.innerHTML += ' ' + passWord
 
-colorPreferitoHTMLElement.innerHTML += ' ' + colorPreferito
+// colorPreferitoHTMLElement.innerHTML += ' ' + colorPreferito
 
-ageHTMLElement.innerHTML += ' ' + birth + ' ' + 'anni'
+// ageHTMLElement.innerHTML += ' ' + birth + ' ' + 'anni'
 
 // colorPreferitoHTMLElement.innerHTML += ` ${colorPreferito}`  // modo alternativo per scrivere riga36
 
